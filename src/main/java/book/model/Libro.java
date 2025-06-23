68e831bbbbf0feb3e9eebfbfea69afd2c8f27c02
Rafael169 <r.alvarado1693@gmail.com>
2025-06-23 16:37:30 -0500

    agregacion de validacion para evitar campos null

--- src/main/java/book/model/Libro.java
+++ src/main/java/book/model/Libro.java
@@ -3,10 +3,7 @@ package book.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
 
-
-import java.util.Date;
 @Entity
 public class Libro {
     @Id
@@ -25,6 +22,7 @@ public class Libro {
 
     @NotNull(message = "El stock es obligatorio")
     private Integer stock;
+
 
     public Libro() {
     }
