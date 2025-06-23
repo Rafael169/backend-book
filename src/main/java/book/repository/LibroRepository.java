61f2b62fa91738cd02caf4fc0214e1e6fea88831
Rafael169 <r.alvarado1693@gmail.com>
2025-06-23 13:34:44 -0500

    Agregacion de ADML en el repo

--- src/main/java/book/repository/LibroRepository.java
+++ src/main/java/book/repository/LibroRepository.java
@@ -2,7 +2,10 @@ package book.repository;
 
 
 
+import book.model.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public class LibroRepository {
+@Repository
+public interface LibroRepository extends JpaRepository<Libro, Long> {
 }
